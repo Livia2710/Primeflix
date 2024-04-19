39dc18ca8027ad512684bb958b9aa35a6feea7bd
@Livia2710 <livia.sesi166@gmail.com>
2024-04-19 11:00:02 -0300

    wert

---
+++ primeflix/src/pages/Movie/index.js
@@ -0,0 +1,39 @@
+import { memo, useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faTv, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+import imageNotFound from '../../assets/images/placeholder.png';
+import placeholderImage from '../../assets/glyphicons/picture-grey.svg';
+
+import { Button, Loading } from '../../components'; // Importação de componentes
+import { toast } from 'react-toastify'; // Importação do módulo de notificações
+
+import 'react-lazy-load-image-component/src/effects/blur.css'; // Importação de estilos
+
+import styles from './styles.module.css'; // Importação dos estilos locais
+import api from '../../services/api'; // Importação do serviço de API
+
+function Movie() {
+    const { id } = useParams(); //Obtém o ID do filme da URL
+    const navigate = useNavigate(); //Função de navegação
+
+    const [movie, setMovie] = useState({}); //Armazea os detlahes do filme
+    const [loading, setLoading] = useState(true); //Controlar o carrgamento
+
+    useEffect(() => {
+        async function loadMovie() {
+            try {
+                //Requisição para obter detalhes do filme pelo ID
+                const response = await api.get(`movie/${id}`);
+                //Atualiza o estado com os detlahes do filme e finaliza o carregamento
+                setMovie(response.data);
+                setLoading(false);
+            } catch (error) {
+                //Em caso de erro, redireciona de volta para a página inicial
+                navigate('/', {replace: true});
+            }
+        }
+    })
+ }
